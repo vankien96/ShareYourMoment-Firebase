401942250bc87762b3d62209b91f19fdf11abcb2
TPPFIT iCloud <KienTruong@tppfits-mac-mini.local>
2017-08-01 16:05:26 +0700

    Initial Commit

---
+++ DemoMaterialApp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  DemoMaterialApp
+//
+//  Created by TPPFIT iCloud on 8/1/17.
+//  Copyright © 2017 TPPFIT iCloud. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
